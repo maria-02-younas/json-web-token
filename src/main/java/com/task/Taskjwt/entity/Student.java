c7d63e1742e01de78a491792d6d85ef131a41105
maria.younas <maria.mehmood@paysyslabs.com>
2025-05-06 16:36:20 +0500

    add lombok

--- src/main/java/com/task/Taskjwt/entity/Student.java
+++ src/main/java/com/task/Taskjwt/entity/Student.java
@@ -1,7 +1,12 @@
 package com.task.Taskjwt.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "student")
 public class Student {
@@ -18,51 +23,6 @@ public class Student {
 
     @Column(name = "email_address")
     private String emailAddress;
-
-//    Constructors
-
-    public Student() {
-    }
-
-    public Student(String firstName, String lastName, String emailAddress) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailAddress = emailAddress;
-    }
-
-//    Setters & Getters
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
 
     @Override
     public String toString() {
